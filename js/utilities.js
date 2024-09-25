f73caec28b6cf8ece11e4dd2c0fb7ef0b206b26c
Muhammad Masud Parvez <masudparvez.me@gmail.com>
2024-09-25 12:46:46 +0600

    ading button color

--- js/utilities.js
+++ js/utilities.js
@@ -12,9 +12,16 @@ function getTextValueById(id) {
 document.getElementById("show-donation").addEventListener("click", function () {
   document.getElementById("donation-cards").classList.remove("hidden");
   document.getElementById("history-section").classList.add("hidden");
+  document.getElementById("show-donation").classList.add("bg-[#a3e635]");
+  document.getElementById("show-history").classList.remove("bg-[#a3e635]");
+  document.getElementById("show-history").classList.add("border-gray-300");
 });
 
 document.getElementById("show-history").addEventListener("click", function () {
   document.getElementById("history-section").classList.remove("hidden");
   document.getElementById("donation-cards").classList.add("hidden");
+  document.getElementById("show-donation").classList.remove("bg-[#a3e635]");
+  document.getElementById("show-donation").classList.add("border-gray-300");
+  document.getElementById("show-history").classList.add("bg-[#a3e635]");
+  document.getElementById("show-history").classList.remove("border-gray-300");
 });
