ab230380c84df0b2885c0f95115782c56c71d4cf
Muhammad Masud Parvez <masudparvez.me@gmail.com>
2024-09-25 11:03:14 +0600

    ading new

--- js/utilities.js
+++ js/utilities.js
@@ -1,10 +1,20 @@
 function getInputValueById(id) {
-  const inputValue = document.getElementById("input" + id).value;
+  const inputValue = document.getElementById(id).value;
   const inputNumber = parseFloat(inputValue);
   return inputNumber;
 }
 function getTextValueById(id) {
-  const textValue = document.getElementById("input" + id).innerText;
+  const textValue = document.getElementById(id).innerText;
   const textNumber = parseFloat(textValue);
   return textNumber;
 }
+
+document.getElementById("show-donation").addEventListener("click", function () {
+  document.getElementById("donation-cards").classList.remove("hidden");
+  document.getElementById("history-section").classList.add("hidden");
+});
+
+document.getElementById("show-history").addEventListener("click", function () {
+  document.getElementById("history-section").classList.remove("hidden");
+  document.getElementById("donation-cards").classList.add("hidden");
+});
